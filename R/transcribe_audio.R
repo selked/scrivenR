40fbb1e874e552a8d3bb7c0db89d34121ed99007
selked <selked@mail.gvsu.edu>
2025-09-17 13:32:40 -0400

    Adding CPU core detection for parallel processing

--- R/transcribe_audio.R
+++ R/transcribe_audio.R
@@ -4,18 +4,21 @@
 #' @param x Output of list.files() in working directory containing videos
 #' @param include_timing Option to include timestamps for each line of text, default=TRUE
 #' @param internal_convert Option to retain naming format for conversions made with this package, default=TRUE
+#' @param all_cores Use all available CPU cores for parallel processing (otherwise, half will be used), default=FALSE
 #' @keywords transcription, whisper, ASR, batch-processing
 #' @export
 #' @examples
 #' transcribe_audio(list.files(), include_timing=TRUE, internal_convert=TRUE)
 
 
-transcribe_audio <- function(x, include_timing=TRUE, internal_convert=TRUE, model_path) {
+transcribe_audio <- function(x, include_timing=TRUE, internal_convert=TRUE, model_path, all_cores = FALSE) {
 
 if (missing(model_path)) {
   print("Path to Whisper acoustic model must be specified. Enter the directory containing the model downloaded with audio.whisper, or use its whisper() function to download one of the acoustic models and enter that path here.")
 }
   else {
+
+n_cores <- as.numeric(detectCores())
 
 model <- whisper(model_path)
 
@@ -39,14 +42,30 @@ if (internal_convert==TRUE) {
 
     print(paste0("Transcribing ", fn, "..."))
 
+    if (all_cores == TRUE) {
+
     tic()
     trans <- predict(model,
                      newdata = fn,
                      language = "en",
-                     n_threads = 8)
+                     n_threads = n_cores)
     toc()
 
     print(paste0("Finished transcribing ", fn, "."))
+}
+
+    else {
+
+      tic()
+      trans <- predict(model,
+                       newdata = fn,
+                       language = "en",
+                       n_threads = ceiling((n_cores/2)))
+      toc()
+
+      print(paste0("Finished transcribing ", fn, "."))
+    }
+
 
     speech <- trans$data |>
       select(text)

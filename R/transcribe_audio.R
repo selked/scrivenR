1ca30e3da1e943d52e9cd05aea299716d4b8a480
Daulton Selke <selked@mail.gvsu.edu>
2025-12-04 23:58:13 -0500

    Test of unadjusted duration after df_to_tier fix

--- R/transcribe_audio.R
+++ R/transcribe_audio.R
@@ -105,7 +105,7 @@ if (internal_convert==TRUE) {
       dur <- dur[1,2]
 
       create_empty_textgrid(
-        duration = dur+10,
+        duration = dur,
         point_tier = NULL,
         path = getwd(),
         result_file_name = paste0(file_path_sans_ext(fn))

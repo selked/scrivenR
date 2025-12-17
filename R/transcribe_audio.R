267454cce205eab4d03958aba0ff90f419e70f1d
selked <selked@mail.gvsu.edu>
2025-12-16 19:54:07 -0500

    Removing initial hyphens for TextGrids

--- R/transcribe_audio.R
+++ R/transcribe_audio.R
@@ -85,6 +85,7 @@ if (internal_convert==TRUE) {
       names(chk)[4] <- "content"
 
       chk$content <- gsub("\"", "", chk$content, fixed = TRUE)
+      chk$content <- gsub("$-", "", chk$content, fixed = TRUE)
 
       chk <- chk |>
         mutate(time_start = period_to_seconds(hms(from))) |>
